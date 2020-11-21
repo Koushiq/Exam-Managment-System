a30cae081da9787864249fafc44d6460905796f7
MosammatHabibaRahman <habiba9g@yahoo.com>
2020-11-21 22:18:21 +0600

    001 - Student Profile + CourseList + Gradesheet

---
+++ ExamManagementSystem/ExamManagementSystem/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using ExamManagementSystem.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExamManagementSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentRepository student = new StudentRepository();
+        // GET: Student
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(student.Get(1));
+        }
+        
+        [HttpGet]
+        public ActionResult CourseList()
+        {
+            EnrollRepository enr = new EnrollRepository();
+            return View(enr.GetAllByStudentId(1));
+        }
+
+        [HttpGet]
+        public ActionResult Gradesheet()
+        {
+            GradeSheetRepository gsr = new GradeSheetRepository();
+            return View(gsr.GetStudentGradesheet(1));
+        }
+    }
+}
---
+++ ExamManagementSystem/ExamManagementSystem/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using ExamManagementSystem.Models.DataAccess;
+using ExamManagementSystem.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExamManagementSystem.Controllers
+{
+    public class UserController : Controller
+    {
+        UserRepository user = new UserRepository();
+        // GET: User
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(user.GetAll());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(User u)
+        {
+            user.Insert(u);
+            return RedirectToAction("Index");
+        }
+    }
+}
