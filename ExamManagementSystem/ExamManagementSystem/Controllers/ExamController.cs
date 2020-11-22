65ed4bfe5da342300bea35baa33a6fbf90dc59ba
MosammatHabibaRahman <habiba9g@yahoo.com>
2020-11-22 21:20:10 +0600

    003 - Dynamic Answer Script Done

--- ExamManagementSystem/ExamManagementSystem/Controllers/ExamController.cs
+++ ExamManagementSystem/ExamManagementSystem/Controllers/ExamController.cs
@@ -17,19 +17,10 @@ namespace ExamManagementSystem.Controllers
             ExamRepository exam = new ExamRepository();
             ViewBag.Exm = exam.Get(eid);
             ViewBag.Course = exam.Get(eid).Section.Cours.CourseName;
+            
             QuestionRepository qtr = new QuestionRepository();
             List<Question> qts = new List<Question>();
             qts = qtr.GetQuestionsByExamId(eid);
-
-            /*foreach(var q in qts)
-            {
-                if(q.Type=="MCQ" || q.Type=="Check")
-                {
-                    OptionRepository opt = new OptionRepository();
-                    List<Option> op = new List<Option>();
-                    op = opt.GetOptionsByQuestionId(q.Id);
-                }
-            }*/
 
             return View(qts);
         }
