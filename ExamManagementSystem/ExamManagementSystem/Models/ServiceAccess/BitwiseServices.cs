89ea06d0ae8cbbfd7d6c903350bbac27900ad74b
Ikramul Haque Chowdhury <ikramuloli@gmail.com>
2020-11-23 19:10:20 +0600

    added bitwiseServices to access permissions, ansqer options etc

---
+++ ExamManagementSystem/ExamManagementSystem/Models/ServiceAccess/BitwiseServices.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamManagementSystem.Models.ServiceAccess
+{
+    public static class BitwiseServices
+    {
+        public static List<int> GetEnabledIndexList(int integerValue)
+        {
+            List<int> enabledIndecesList = new List<int>();
+            for(int i = 0; i < 32; i++)
+            {
+                if (integerValue.CheckBit(i)) enabledIndecesList.Add(i);
+            }
+            return enabledIndecesList;
+        }
+        public static int GetIntegerValue(List<int> enabledIndecesList)
+        {
+            int integerValue = 0;
+            foreach(int index in enabledIndecesList)
+            {
+                integerValue.SetBit(index);
+            }
+            return integerValue;
+        }
+        public static void SetBit(this int integerValue, int bit)
+        {
+            integerValue = integerValue | (1 << bit);
+        }
+        public static void ClearBit(this int integerValue, int bit)
+        {
+            integerValue = integerValue & (~(1 << bit));
+        }
+        public static bool CheckBit(this int integerValue, int bit)
+        {
+            return ((integerValue & (1 << bit)) != 0);
+        }
+    }
+}
